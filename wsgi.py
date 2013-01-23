071abc145a4762f669e9db49ecbf50f081aa4770
Gilson Filho <gilson@padawan.(none)>
2013-01-22 22:10:51 -0200

    Fazer tudo de novo

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'gilsondev')))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'gilsondev.settings'
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
